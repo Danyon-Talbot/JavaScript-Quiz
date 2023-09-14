58c6ed9c8311b8e3b3caaf472c07a50df2d118d3
danyon talbot <danyon.talbot2@gmail.com>
2023-09-14 23:19:20 +1000

    Final JavaScript code

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,6 +2,8 @@ var answerConfirm = document.getElementsByClassName("answer-check");
 var quizContainer = document.getElementById("answer-container");
 var questionContainer = document.getElementById("question-container");
 var resultsContainer = document.getElementById("results-container");
+var textInfo = document.getElementById("quizInfo");
+var scoreContainer = document.getElementById("final-score")
 var questionCount = 0;
 var correctAnswer = null;
 var secondsLeft = 30;
@@ -39,22 +41,27 @@ document.addEventListener("DOMContentLoaded", function() {
     attachStartListener();
 });
 
+
+
+
 //Adds an event listener to the Start Button
 function attachStartListener() {
     document.addEventListener("click", function(event) {
         if (event.target.id === "start") {
             countdown();
             displayQuestion();
-            event.target.style.display = 'none';
+            event.target.style.display = "none";
+            textInfo.style.display = "none";
         }
     })
 }
+
 
 document.addEventListener("DOMContentLoaded", function() {
     // Wait for the entire document to be loaded before attaching the listener
 
     var topScoreButton = document.getElementById("top-score");
-    if (topScoreButton) { // Make sure the element exists
+    if (topScoreButton) { //Ensures the element exists
         topScoreButton.addEventListener("click", function() {
             displayResults();
 
@@ -92,21 +99,20 @@ function countdown() {
 function displayQuestion() {
     if (questionCount < questions.length) {
         const currentQuestion = questions[questionCount];
-
+        //finds the current displayed question's answers and assigns them to an element
         var choicesHTML = currentQuestion.choices.map((choice, index) =>
             `<li><button class="choice" onclick="checkAnswer(${index})">${choice}</button></li>`
         ).join('');
-
+        //displays current question text
         document.getElementById("question-text").textContent = currentQuestion.question;
-
+        //displays current question answers
         quizContainer.innerHTML = `
             <ul class="choice-list">
                 ${choicesHTML}
             </ul>
         `;
-
+        //sets the correct answer for the current question
         correctAnswer = currentQuestion.correctAnswer;
-        console.log("next question")
     } else {
         //Calls the final score function when the final question has been answered
         finalScore();
@@ -123,9 +129,10 @@ function checkAnswer(selected) {
         displayTimer();
         console.log("Timer-update")
     }
-
+    //var sets If selected is Stictly equal: displays Correct!, else Incorrect!
     var confirmation = (selected === correctAnswerIndex) ? `<h3>Correct!<h3>` : `<h3>Incorrect!</h3>`;
 
+    //Checks selected answer against displayed answers
     answerConfirm = document.getElementsByClassName("answer-check");
 
     for (let i = 0; i < answerConfirm.length; i++) {
@@ -145,7 +152,7 @@ function checkAnswer(selected) {
         }
     }, 2000)
 
-
+    //increments the question number and displays next question
     questionCount ++;
     displayQuestion();
 
@@ -153,19 +160,20 @@ function checkAnswer(selected) {
 
 //Clears Timer, displays final score, and asks user to input Initials
 function finalScore() {
-    console.log("OK");
+    console.log("Final Score");
+    scoreContainer.style.display = "flex";
     clearInterval(timerInterval);
     score = secondsLeft;
 
     //Array.from(answerConfirm).forEach(item => item.innerHTML = "");
-
-    questionContainer.innerHTML = `
+    scoreContainer.innerHTML = `
         <h1 class="question">Final Score: ${secondsLeft}</h1>
         <p class="enter-initials">Enter Initials: <input type="text" id="initials" maxlength="3"/></P>
         <button onclick="submitResults()">Submit</button>
     `;
 
-    quizContainer.innerHTML = "";
+    questionContainer.style.display = "none";
+    quizContainer.style.display = "none";
 
     console.log(secondsLeft);
 }
@@ -180,29 +188,33 @@ function submitResults() {
     }
     
     var savedResults = JSON.parse(localStorage.getItem("Results")) || [];
-
+    //Ensures that all saved results are viable
     if (!Array.isArray(savedResults)) {
         console.warn("Unexpected data in localStorage. Resetting...");
         savedResults = [];
     }
-
+    //Ensures that new results are added to the existing storage and not replaced
     savedResults.push(newResult);
 
     localStorage.setItem("Results", JSON.stringify(savedResults));
     
     displayResults();
-        questionContainer.style.display = 'none';
+    //hides itself
+    scoreContainer.style.display = 'none';
 
 }
 
 //Displays top scores
 function displayResults() {
+    console.log("Display Results")
     var hideHeader = document.getElementById("header")
     resultsContainer.style.display = 'flex';
     hideHeader.style.display = "none";
+    textInfo.style.display = "none";
 
     var storedResults = JSON.parse(localStorage.getItem("Results"));
     var resultsHTML = "";
+    //If results are found, displays them on screen with nav buttons
     if (storedResults && storedResults.length > 0) {
         resultsHTML += '<h2 id="displayScoreTitle">TOP SCORES</h2>';
         resultsHTML += '<button id="reset-quiz" class="resetButton" onclick="resetQuiz()">Home</button>';
@@ -210,6 +222,8 @@ function displayResults() {
         storedResults.forEach(result => {
             resultsHTML += `<ol><li id="results-list">${result.Initials} - ${result.Score}</li></ol>`;
         });
+
+    //if no scores are found, displays "No Results" and nav buttons
     } else {
         resultsHTML += '<h2 id="displayScoreTitle">TOP SCORES</h2>';
         resultsHTML += '<button id="reset-quiz" onclick="resetQuiz()">Home</button>';
@@ -220,43 +234,52 @@ function displayResults() {
     resultsContainer.innerHTML = resultsHTML;
 }
 
+//Function is called when Clear Score button is clicked
 function clearScores() {
     localStorage.removeItem("Results");
     displayResults();
 }
 
+//Resets the quiz back to default settings.
 function resetQuiz() {
+    //defaults variables
     questionCount = 0;
     secondsLeft = 30;
     correctAnswer = null;
     timeEl.textContent = "Time: 0";
     var showHeader = document.getElementById("header");
 
-    questionContainer.style.display = "flex";  
+    //resets display values
+    questionContainer.style.display = "flex";
+    quizContainer.style.display = "flex";
     resultsContainer.style.display = "none";
     showHeader.style.display = "flex";
+    textInfo.style.display = "flex";
+    document.getElementById("start").style.display = "block";
 
+    //resets question text to original header
     questionContainer.innerHTML = `
-        <h1 id="question-text" class="questionText">HEADER</h1>
-        <button id="start">Start</button>
+        <h1 id="question-text" class="questionText">JavaScript Quiz</h1>
     `;
-    
+
+    //resets the quiz container to hidden
     quizContainer.innerHTML = `
             <!--Answers-->
         <div class="answer-check" style="display: none;">
         </div>
     `;
 
+    //resets results to empty
     resultsContainer.innerHTML = `
         <!--Results-->
     `
 
+    //clears any remaining elements in answer check
     var elements = document.getElementsByClassName("answer-check");
     for(var el of elements) {
         el.remove();
     }
 
-    attachStartListener();
 }
 
 
